ef7dbd73e407d5c3801a023dada677676b09e5ec
Cristian De Los Rios <Cris2595@gmail.com>
2022-01-03 09:41:43 -0700

    Resolved issue #27

--- demo/client/components/DocsContainer.tsx
+++ demo/client/components/DocsContainer.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './DocsStyles.css';
 
-// import pic from './assets/githubReadme.png';
+ import pic from './assets/githubReadme.png';
+ 
 
 function DocsContainer() {
   return (
     <div className='github'>
       <h2 className='section-title'>Docs</h2>
-      {/* <a href='https://github.com/oslabs-beta/EmberQL#readme' className='link'>
+      <a href='https://github.com/oslabs-beta/EmberQL#readme' className='link'>
         EmberQL Github
-      </a> */}
-      {/* <img src={pic} alt='Github Readme' height='80%' width='80%' /> */}
+      </a> 
+       <img src={pic} alt='Github Readme' height='80%' width='80%' /> 
     </div>
   );
 }
