1028d84bcd2f75922320db12b0f54c9a9d19937e
manjunathap85 <manjunathap85@gmail.com>
2022-01-06 02:02:49 +0800

    webpack & image issues fixed

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -3,68 +3,66 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: './client/index.tsx',
-  devtool: 'source-map',
-  //mode: "development",
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  devServer: {
-    port: 8080,
-    publicPath: '/build/',
-   
+	mode: process.env.NODE_ENV,
+	entry: './client/index.tsx',
+	devtool: 'source-map',
+	//mode: "development",
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: '/build/',
+	},
+	devServer: {
+		port: 8080,
+		publicPath: '/build/',
 
-    proxy: {
-      '*/': 'http://localhost:3000/',
-    },
-    hot: true,
-    historyApiFallback: true,
-  },
-  //entry: path.resolve(__dirname, './client/index.tsx'),
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)?$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-      },
+		proxy: {
+			'/': 'http://localhost:3000/',
+		},
+		hot: true,
+		historyApiFallback: true,
+	},
+	//entry: path.resolve(__dirname, './client/index.tsx'),
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)?$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
 
-      {
-        test: /\.(js|jsx)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        //exclude: /node_modules/,
-        include: path.resolve(__dirname, 'client'),
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /.(png|svg|jpg|gif|woff|ico|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-      chunkFilename: 'styles.css',
-     
-    }),
-  ],
+			{
+				test: /\.(js|jsx)?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react', '@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				//exclude: /node_modules/,
+				include: path.resolve(__dirname, 'client'),
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /.(png|svg|jpg|gif|woff|ico|woff2|eot|ttf|otf)$/,
+				use: ['file-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'styles.css',
+			chunkFilename: 'styles.css',
+		}),
+	],
 };
